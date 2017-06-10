4d5f5f61efe3f18059e67ddaf7cb04a4d12a6491
Goerge Albrecht <goerge@code-mentor.com>
2017-06-10 15:07:17 +0200

    session wth Bernie

--- src/test/java/Snake.java
+++ src/test/java/Snake.java
@@ -1,11 +1,55 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class Snake {
-  private List<Element> elements = Arrays.asList(
-    new Element(new Position(4, 4)),
-    new Element(new Position(5, 4)),
-    new Element(new Position(6, 4)));
+  private MovingDirection movingDirection;
+
+  public void setMovingDirection(MovingDirection movingDirection) {
+    if(movingDirection.isOppositeOf(this.movingDirection)) {
+      throw new IllegalStateException();
+    }
+    this.movingDirection = movingDirection;
+  }
+
+  public MovingDirection getMovingDirection() {
+    return movingDirection;
+  }
+
+  public enum MovingDirection {
+    UP {
+      @Override
+      public boolean isOppositeOf(MovingDirection movingDirection) {
+        return movingDirection == DOWN;
+      }
+    }, DOWN {
+      @Override
+      public boolean isOppositeOf(MovingDirection movingDirection) {
+        return movingDirection == UP;
+      }
+    }, RIGHT {
+      @Override
+      public boolean isOppositeOf(MovingDirection movingDirection) {
+        return movingDirection == LEFT;
+      }
+    }, LEFT {
+      @Override
+      public boolean isOppositeOf(MovingDirection movingDirection) {
+        return movingDirection == RIGHT;
+      }
+    };
+
+    public abstract boolean isOppositeOf(MovingDirection movingDirection);
+  }
+
+  private List<Element> elements = new ArrayList<>();
+
+  {
+    elements.addAll(Arrays.asList(
+      new Element(new Position(4, 4)),
+      new Element(new Position(5, 4)),
+      new Element(new Position(6, 4))));
+  }
 
   public List<Element> getElements() {
     return elements;
--- src/test/java/SnakeGameTest.java
+++ src/test/java/SnakeGameTest.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SnakeGameTest {
   @Test
@@ -27,5 +31,25 @@ class SnakeGameTest {
   void elementHasPosition() {
     final Element element = new Element(new Position(1, 2));
     assertEquals(element.position(), new Position(1, 2));
+  }
+
+  @ParameterizedTest()
+  @EnumSource(Snake.MovingDirection.class)
+  void setMovingDirection(Snake.MovingDirection direction) {
+    final Snake snake = new Snake();
+    final List<Element> elements = new ArrayList<>(snake.getElements());
+
+    snake.setMovingDirection(direction);
+
+    assertEquals(snake.getMovingDirection(), direction);
+    assertEquals(snake.getElements(), elements);
+  }
+
+  @Test
+  void setMovingDirectionOpposite() {
+    final Snake snake = new Snake();
+    snake.setMovingDirection(Snake.MovingDirection.UP);
+    assertThrows(IllegalStateException.class,()->snake.setMovingDirection(Snake.MovingDirection.DOWN));
+    assertEquals(snake.getMovingDirection(), Snake.MovingDirection.UP);
   }
 }
